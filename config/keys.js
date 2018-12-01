b9d7190e1af16b442c4b0c1e6edb7f33cce42df7
Chandni Mishra <chandni.mishra@redbus.local>
2018-12-01 17:45:11 +0530

    Initail express server with routes file

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://chandtushar:Testing123@ds151602.mlab.com:51602/projectnew"
+};
---
+++ routes/api/users.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+//@route GET api/users/test
+//@desc Tests Post Route
+//@access Public
+router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+module.exports = router;
